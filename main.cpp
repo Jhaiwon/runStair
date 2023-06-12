aca9c1aed31892d1761d3b300f87dc3c14e028e2
haiwon.Jeong <hainni120213@gmail.com>
2023-06-12 10:20:14 +0900

    기반

--- main.cpp
+++ main.cpp
@@ -1,19 +1,47 @@
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 
-class Player{
+class Player
+{
     int level;
-    
-    private:
-    void run();
+    char keyboard_;
+
+public:
+    int run();
+    int moveLeft();
+    int moveRight();
 };
 
-void Player::run()
+int Player::run()
 {
+    if (keyboard_ = 'a')
+    {
+        system("cls");
+        moveLeft();
+    }
+    else if (keyboard_ = 'd')
+    {
+        system("cls");
+        moveRight();
+    }
+    return 0;
 }
 
+int Player::moveLeft()
+{
+    cout<<"왼쪽입니다."<<endl;
+    return 0;
+}
 
-void main(){
-    
-    Player run();
+int Player::moveRight()
+{
+    cout<<"오른쪽입니다."<<endl;
+    return 0;
+}
+
+int main()
+{
+    Player Pl;
+    Pl.run();
 }
