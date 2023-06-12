b11d6190d993a7d11d06ade7b94ca84c96ab9a7c
haiwon.Jeong <hainni120213@gmail.com>
2023-06-12 10:00:30 +0900

    더하기

--- main.cpp
+++ main.cpp
@@ -1,3 +1,19 @@
 #include <iostream>
 using namespace std;
 
+class Player{
+    int level;
+    
+    private:
+    void run();
+};
+
+void Player::run()
+{
+}
+
+
+void main(){
+    
+    Player run();
+}
